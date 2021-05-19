f079bfad2ef140cc60839264f0ce9ce356a6c052
Taylor Buckner <taylora.buckner@gmail.com>
2021-05-19 08:18:30 -0500

    fix(logger): removed dist code, added cleanup scripts.

--- dist/util/command-handler.d.ts
+++
@@ -1,7 +0,0 @@
-import { LiquibaseConfig } from '../models';
-import { Logger } from '../util';
-export declare class CommandHandler {
-    logger: Logger;
-    constructor(config: LiquibaseConfig);
-    spawnChildProcess(commandString: string): Promise<string>;
-}
